bad37b32f796745a108a3184992ee7362f76cf82
cesarnavarro1011 <cesarnavarro1011+github@gmail.com>
2025-08-11 21:32:11 -0500

    Remove dist from repository tracking

--- dist/app.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import PhoneInputWithPrefixes from "./phone-input-prefixes";
-export default function App() {
-    return (_jsx("div", { className: "p-4", children: _jsx(PhoneInputWithPrefixes, { value: "", onChange: (value, code) => console.log(value, code), countryDefault: "co", enableSearch: true }) }));
-}
